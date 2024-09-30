3b2cfb6c9f185f987cd9ad09c5575421f68f546a
Kumar Anurag <81381360+10kumaranurag01@users.noreply.github.com>
2024-09-30 13:06:46 +0530

    Delete dist directory

--- dist/routes/helperRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const helperController_1 = require("../controllers/helperController");
-const router = (0, express_1.Router)();
-router.get("/all-books", helperController_1.getAllBooks);
-router.get("/all-users", helperController_1.getAllUsers);
-exports.default = router;
